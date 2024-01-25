e3817527c7b45cc24f973c7862107b8f39b03f7e
yuanz <yuanz271@gmail.com>
2024-01-25 17:09:25 -0500

    follow XFADS spec

--- src/bfs/smoothing.py
+++ src/bfs/smoothing.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import partial
 from typing import Type
 from equinox import Module
 from jaxtyping import Array, PRNGKeyArray
@@ -17,6 +18,7 @@ def get_obs_encoder(
     observation_dim: int,
     hidden_size: int,
     n_layers: int,
+    approx: Type[ExponentialFamily],
     *,
     key: PRNGKeyArray,
 ):
@@ -34,7 +36,7 @@ def get_back_encoder(
 
 
 def constrained_natural(unconstrained):
-    nat1, nat2 = jnp.split(unconstrained, 2)
+    nat1, nat2 = jnp.split(unconstrained, 2, axis=-1)
     nat2 = -jnn.softplus(nat2)
     return jnp.concatenate((nat1, nat2), axis=-1)
 
@@ -60,50 +62,54 @@ def smooth(
     hyperparam: Hyperparam,
 ) -> tuple[Array, Array]:
     approx = hyperparam.approx
-    nature_f_1 = constrained_natural(obs_encoder(y[0]))
+    nature_y = constrained_natural(jax.vmap(obs_encoder)(y))
+    nature_f_1 = nature_y[0]
+    moment_f_1 = approx.natural_to_moment(nature_f_1)
+
+    expected_moment = partial(sample_expected_moment, forward=dynamics, noise=statenoise, approx=approx, mc_size=hyperparam.mc_size)
+    natural_to_moment = jax.vmap(approx.natural_to_moment)
 
     def forward(carry, obs):
         key, nature_f_tm1 = carry
-        key, key_t = jrandom.split(key, 2)
-        y, u = obs
+        subkey, key_t = jrandom.split(key, 2)
+        nature_y_t, u = obs
         moment_s_tm1 = approx.natural_to_moment(nature_f_tm1)
-        moment_p_t = sample_expected_moment(
-            key_t, dynamics, statenoise, moment_s_tm1, u, approx, hyperparam.mc_size
-        )
+        moment_p_t = expected_moment(key_t, moment_s_tm1, u)
         nature_p_t = approx.moment_to_natural(moment_p_t)
-        update = constrained_natural(obs_encoder(y))
-        nature_f_t = nature_p_t + update
-        return (key, nature_f_t), (nature_p_t, nature_f_t)
+        nature_f_t = nature_p_t + nature_y_t
+        return (subkey, nature_f_t), (moment_p_t, nature_p_t, nature_f_t)
 
     def backward(carry, z):
         key, nature_s_tp1 = carry
-        key, key_t = jrandom.split(key, 2)
-        nature_f_t, u = z
+        subkey, key_t = jrandom.split(key, 2)
+        nature_y_t, nature_f_t = z
 
-        update = constrained_natural(back_encoder(nature_s_tp1))
+        update = constrained_natural(back_encoder(jnp.concatenate((nature_y_t, nature_s_tp1), axis=-1)))
         nature_s_t = nature_f_t + update
-        moment_s_t = approx.natural_to_moment(nature_s_t)
-        moment_p_tp1 = sample_expected_moment(
-            key_t, dynamics, statenoise, moment_s_t, u, approx, hyperparam.mc_size
-        )
-        return (key, nature_s_t), (moment_p_tp1, nature_s_t)
+        return (subkey, update), nature_s_t
 
-    key, key_f = jrandom.split(key, 2)
-    _, (nature_p, nature_f) = scan(
-        forward, init=(key_f, nature_f_1), xs=(y[1:], u[1:])
+    key, forward_key = jrandom.split(key, 2)
+    _, (moment_p, nature_p, nature_f) = scan(
+        forward, init=(forward_key, nature_f_1), xs=(nature_y[1:], u[1:])
     )  #
-    nature_p = jnp.vstack((nature_f_1, nature_p))
+    
+    moment_p = jnp.vstack((moment_f_1, moment_p))
     nature_f = jnp.vstack((nature_f_1, nature_f))
+    moment_s = natural_to_moment(nature_f)
 
-    nature_s_T = nature_f[-1]
-    key, key_b = jrandom.split(key, 2)
-    _, (moment_p, nature_s) = scan(
-        backward, init=(key_b, nature_s_T), xs=(nature_f[:-1], u[:-1]), reverse=True
+    ## Backward
+    nature_s_T = nature_f[-1]   
+    update_s_T = jnp.zeros_like(nature_s_T)
+    key, backward_key = jrandom.split(key, 2)
+    _, nature_s = scan(
+        backward, init=(backward_key, update_s_T), xs=(nature_y[:-1], nature_f[:-1]), reverse=True
     )  # reverse both xs and the output
     nature_s = jnp.vstack((nature_s, nature_s_T))
-    moment_s = jax.vmap(approx.natural_to_moment)(nature_s)
+    moment_s = natural_to_moment(nature_s)
     moment_s_1 = moment_s[0]
-    moment_p = jnp.vstack((moment_s_1, moment_p))
-    nature_p = jax.vmap(approx.moment_to_natural)(moment_p)
+    
+    keys = jrandom.split(key, jnp.size(moment_s, 0))
+    moment_p = jax.vmap(expected_moment)(keys, moment_s, u)
+    moment_p = jnp.vstack((moment_s_1, moment_p[:-1]))
 
     return moment_s, moment_p

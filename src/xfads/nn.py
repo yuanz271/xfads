c6e03eeac1b5ce293fd9430c77933e94b0e559ec
yz <yuanz271@gmail.com>
2024-03-11 16:56:33 -0700

    allow 0 hidden layer MLP (linear)

--- src/xfads/nn.py
+++ src/xfads/nn.py
@@ -4,13 +4,14 @@ from jax import nn as jnn, random as jrandom, numpy as jnp
 from equinox import nn as enn
 
 
-def make_mlp(in_size, out_size, hidden_size, n_layers, *, key: PRNGKeyArray, activation: Callable=jnn.selu):
-    keys = jrandom.split(key, n_layers + 2)
-    layers = [enn.Linear(in_size, hidden_size, key=keys[0]), enn.Lambda(activation)]
+def make_mlp(in_size, out_size, hidden_size, n_layers, *, key: PRNGKeyArray, activation: Callable=jnn.silu):
+    keys = jrandom.split(key, n_layers + 1)
+    layers = []
     for i in range(n_layers):
-        layers.append(enn.Linear(hidden_size, hidden_size, key=keys[i + 1]))
-        layers.append(enn.Lambda(jnn.silu))
-    layers.append(enn.Linear(hidden_size, out_size, key=keys[-1]))
+        layers.append(enn.Linear(in_size, hidden_size, key=keys[i]))
+        layers.append(enn.Lambda(activation))
+        in_size = hidden_size
+    layers.append(enn.Linear(in_size, out_size, key=keys[-1]))
     return enn.Sequential(layers)
 
 

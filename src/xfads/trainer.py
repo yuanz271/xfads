3c683f8042be6f4f2d190cd7f489c2ce015fabfb
yuanz <yuanz271@gmail.com>
2024-03-24 14:52:37 -0400

    refactor

--- src/xfads/trainer.py
+++ src/xfads/trainer.py
@@ -1,10 +1,11 @@
 from collections.abc import Generator
 from dataclasses import dataclass
+from functools import partial
 
 import jax
 from jax import numpy as jnp, random as jrandom
 from tqdm import trange
-from jaxtyping import Array, PRNGKeyArray, Scalar
+from jaxtyping import Array, PRNGKeyArray, PyTree, Scalar
 import chex
 import optax
 import equinox as eqx
@@ -23,12 +24,12 @@ class Opt:
     static: str = ""
 
 
-def make_optimizer(module, opt: Opt):
+def make_optimizer(trainable_modules, opt: Opt):
     optimizer = optax.chain(
         optax.clip_by_global_norm(opt.clip_norm),
         optax.adamw(opt.learning_rate),
     )
-    opt_state = optimizer.init(eqx.filter(module, eqx.is_inexact_array))
+    opt_state = optimizer.init(eqx.filter(trainable_modules, eqx.is_inexact_array))
 
     return optimizer, opt_state
 
@@ -69,15 +70,15 @@ def batch_loss(modules, y, u, x, key, hyperparam) -> Scalar:
     return -elbo(lkey, moment_s, moment_p, covariate_predict, y)
 
 
-def train_loop(modules, y, u, x, key, step, opt_state, opt):
+def train_loop(trainable_modules, static_modules, y, u, x, key, step, opt_state, opt):
     old_loss = jnp.inf
     for i in range(opt.max_inner_iter):
         key_i = jrandom.fold_in(key, i)
         total_loss = 0.
         n_minibatch = 0
         for ym, um, xm, minibatch_key in loader(y, u, x, batch_size=opt.batch_size, key=key_i):
-            modules, opt_state, loss = step(
-                modules, ym, um, xm, minibatch_key, opt_state
+            trainable_modules, opt_state, loss = step(
+                trainable_modules, static_modules, ym, um, xm, minibatch_key, opt_state
             )
             # chex.assert_tree_all_finite(loss)
             total_loss += loss
@@ -88,7 +89,7 @@ def train_loop(modules, y, u, x, key, step, opt_state, opt):
             break
 
         old_loss = total_loss
-    return total_loss, modules, opt_state
+    return total_loss, trainable_modules, opt_state
 
 
 def train_em(
@@ -112,26 +113,17 @@ def train_em(
     optimizer_e, opt_state_estep = make_optimizer(e_modules, opt)
 
     @eqx.filter_value_and_grad
-    def eloss(e_modules, y, u, x, key) -> Scalar:
-        return batch_loss(m_modules | e_modules, y, u, x, key, hyperparam)
+    def _loss(trainable_modules, static_modules, y, u, x, key) -> Scalar:
+        return batch_loss(trainable_modules | static_modules, y, u, x, key, hyperparam)
+    
+    def _step(trainable_modules, static_modules, y, u, x, key, opt_state, loss_func, optimizer) -> tuple:
+        loss, grads = loss_func(trainable_modules, static_modules, y, u, x, key)
+        updates, opt_state = optimizer.update(grads, opt_state, trainable_modules)
+        trainable_modules = eqx.apply_updates(trainable_modules, updates)
+        return trainable_modules, opt_state, loss
 
-    @eqx.filter_value_and_grad
-    def mloss(m_modules, y, u, x, key) -> Scalar:
-        return batch_loss(m_modules | e_modules, y, u, x, key, hyperparam)
-
-    @eqx.filter_jit
-    def estep(module, y, u, x, key, opt_state):
-        loss, grads = eloss(module, y, u, x, key)
-        updates, opt_state = optimizer_e.update(grads, opt_state, module)
-        module = eqx.apply_updates(module, updates)
-        return module, opt_state, loss
-
-    @eqx.filter_jit
-    def mstep(module, y, u, x, key, opt_state):
-        loss, grads = mloss(module, y, u, x, key)
-        updates, opt_state = optimizer_m.update(grads, opt_state, module)
-        module = eqx.apply_updates(module, updates)
-        return module, opt_state, loss
+    estep = eqx.filter_jit(partial(_step, loss_func=_loss, optimizer=optimizer_e))
+    mstep = eqx.filter_jit(partial(_step, loss_func=_loss, optimizer=optimizer_m))
 
     key, em_key = jrandom.split(key)
     old_loss = jnp.inf
@@ -140,10 +132,10 @@ def train_em(
         try:
             ekey, mkey = jrandom.split(jrandom.fold_in(em_key, i))
             loss_e, e_modules, opt_state_estep = train_loop(
-                e_modules, y, u, x, ekey, estep, opt_state_estep, opt
+                e_modules, m_modules, y, u, x, ekey, estep, opt_state_estep, opt
             )
             loss_m, m_modules, opt_state_mstep = train_loop(
-                m_modules, y, u, x, mkey, mstep, opt_state_mstep, opt
+                m_modules, e_modules, y, u, x, mkey, mstep, opt_state_mstep, opt
             )
             loss = 0.5 * (loss_e.item() + loss_m.item())
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -13,17 +13,17 @@ def spec():
         approx="DiagMVN",
         mc_size=10,
         seed=0,
-        dyn_spec={"module": "Nonlinear", "depth": 2, "width": 2, "state_noise": 1.0},
-        lik_spec={"emission_noise": 1.0},
+        dyn_spec={"module": "Nonlinear", "depth": 2, "width": 2, "state_noise": 10.0},
+        lik_spec={"emission_noise": 10.0},
         enc_spec={"depth": 2, "width": 2},
         opt_spec={
-            "mode": "em",
+            "mode": "joint",
             "max_inner_iter": 10,
             "max_outer_iter": 10,
             "learning_rate": 1e-3,
             "clip_norm": 1.0,
             "batch_size": 1,
-            "static": "",
+            "static": "sl",
         },
     )
 

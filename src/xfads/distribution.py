d7df4390141b12fce41a4bea00e6cfc5fe1aa413
yuanz <yuanz271@gmail.com>
2024-08-28 11:02:59 -0400

    low rank should be covariance instead of precision

--- src/xfads/distribution.py
+++ src/xfads/distribution.py
@@ -7,7 +7,6 @@ from typing import Protocol
 from jax import nn as jnn, numpy as jnp, random as jrandom
 from jaxtyping import Array, Scalar, PRNGKeyArray
 import tensorflow_probability.substrates.jax.distributions as tfp
-import chex
 
 from .nn import make_mlp
 
@@ -19,8 +18,8 @@ def _inv(a):
     return jnp.linalg.inv(a + EPS * jnp.eye(a.shape[-1]))
 
 
-class ExponentialFamily(Protocol):
-    """Interface of exponential-family distributions
+class MVN(Protocol):
+    """Interface of MVN distributions
     The subclasses should implement class functions performing
     converion between natural parameter and mean parameter.
     The subclasses should be stateless.
@@ -41,8 +40,6 @@ class ExponentialFamily(Protocol):
     @classmethod
     def kl(cls, moment1, moment2) -> Scalar: ...
 
-
-class MVN(ExponentialFamily):
     @classmethod
     def moment_to_canon(cls, moment: Array) -> tuple[Array, Array]: ...
 
@@ -53,8 +50,11 @@ class MVN(ExponentialFamily):
     def full_cov(cls, cov: Array) -> Array: ...
 
     @classmethod
-    def constrain_natural(cls, unconstrained: Array) -> Array: ...
-    
+    def constrain_moment(cls, unconstrained: Array) -> Array: ...
+
+    @classmethod
+    def noise_moment(cls, noise_cov) -> Array: ...
+
 
 class FullMVN(MVN):
     @classmethod
@@ -65,32 +65,34 @@ class FullMVN(MVN):
         nat1, nat2 = jnp.split(natural, [m])
         p = -2 * nat2  # vectorized precision
         P = jnp.reshape(p, (m, m))  # precision matrix
-        mean = jnp.linalg.solve(P, nat1)
-        moment = jnp.concatenate((mean, p))
+        loc = jnp.linalg.solve(P, nat1)
+        V = _inv(P)
+        v = V.flatten()
+        moment = jnp.concatenate((loc, v))
         return moment
 
     @classmethod
     def moment_to_natural(cls, moment: Array) -> Array:
-        mean, P = cls.moment_to_canon(moment)
+        loc, V = cls.moment_to_canon(moment)
+        P = _inv(V)
         Nat2 = -0.5 * P
         nat2 = Nat2.flatten()
-        nat1 = P @ mean
+        nat1 = P @ loc
         natural = jnp.concatenate((nat1, nat2))
         return natural
 
     @classmethod
     def sample_by_moment(cls, key: PRNGKeyArray, moment: Array, mc_size: int) -> Array:
-        mean, P = cls.moment_to_canon(moment)
-        cov = _inv(P)
-        return jrandom.multivariate_normal(key, mean, cov, shape=(mc_size,))  # It seems JAX does reparameterization trick
+        loc, V = cls.moment_to_canon(moment)
+        return jrandom.multivariate_normal(key, loc, V, shape=(mc_size,))  # It seems JAX does reparameterization trick
 
     @classmethod
     def moment_to_canon(cls, moment: Array) -> tuple[Array, Array]:
         n = jnp.size(moment)
         m = cls.variable_size(n)
-        mean, p = jnp.split(moment, [m])
-        P = jnp.reshape(p, (m, m))
-        return mean, P
+        loc, v = jnp.split(moment, [m])
+        V = jnp.reshape(v, (m, m))
+        return loc, V
     
     @classmethod
     def variable_size(cls, param_size: int) -> int:
@@ -105,16 +107,16 @@ class FullMVN(MVN):
         return int(math.sqrt(param_size))
 
     @classmethod
-    def canon_to_moment(cls, mean: Array, P: Array) -> Array:
-        p = P.flatten()
-        moment = jnp.concatenate((mean, p))
+    def canon_to_moment(cls, mean: Array, V: Array) -> Array:
+        v = V.flatten()
+        moment = jnp.concatenate((mean, v))
         return moment
     
     @classmethod
     def kl(cls, moment1: Array, moment2: Array) -> Scalar:
-        m1, P1 = cls.moment_to_canon(moment1)
-        m2, P2 = cls.moment_to_canon(moment2)
-        return tfp.kl_divergence(tfp.MultivariateNormalFullCovariance(m1, _inv(P1)), tfp.MultivariateNormalFullCovariance(m2, _inv(P2)))
+        m1, V1 = cls.moment_to_canon(moment1)
+        m2, V2 = cls.moment_to_canon(moment2)
+        return tfp.kl_divergence(tfp.MultivariateNormalFullCovariance(m1, V1), tfp.MultivariateNormalFullCovariance(m2, V2))
 
     @classmethod
     def param_size(cls, state_dim: int) -> int:
@@ -126,21 +128,21 @@ class FullMVN(MVN):
         return cls.moment_to_natural(moment)
 
     @classmethod
-    def full_cov(cls, P: Array) -> Array:
-        return _inv(P)
+    def full_cov(cls, V: Array) -> Array:
+        return V
     
     @classmethod
-    def constrain_natural(cls, unconstrained: Array) -> Array:
+    def constrain_moment(cls, unconstrained: Array) -> Array:
         n = jnp.size(unconstrained)
         # n = m + m + m
         # m = sqrt(n + 1) - 1
         m = n // 3
 
-        nat1, diag, nat2 = jnp.split(unconstrained, [m, m + m])
-        L = jnp.outer(nat2, nat2)
-        N2 = - (jnp.diag(jnn.softplus(diag)) + L)  # ND
-        nat2 = N2.flatten()
-        return jnp.concatenate((nat1, nat2))
+        loc, diag, lora = jnp.split(unconstrained, [m, m + m])
+        L = jnp.outer(lora, lora)
+        V = jnp.diag(jnn.softplus(diag)) + L
+        v = V.flatten()
+        return jnp.concatenate((loc, v))
 
     @classmethod
     def get_encoders(cls, observation_dim, state_dim, depth, width, key) -> tuple:
@@ -157,92 +159,24 @@ class FullMVN(MVN):
             key=back_key,
         )
         return obs_enc, back_enc
-
-
-class LoRaMVN(MVN):
-    @classmethod
-    def natural_to_moment(cls, natural: Array) -> Array:
-        """Pmu, -P/2 => mu, P"""
-        n = jnp.size(natural)
-        m = cls.variable_size(n)
-        nat1, nat2 = jnp.split(natural, [m])
-        p = -2 * nat2  # vectorized precision
-        P = jnp.reshape(p, (m, m))  # precision matrix
-        mean = jnp.linalg.solve(P, nat1)
-        moment = jnp.concatenate((mean, p))
-        return moment
-
-    @classmethod
-    def moment_to_natural(cls, moment: Array) -> Array:
-        mean, P = cls.moment_to_canon(moment)
-        Nat2 = -0.5 * P
-        nat2 = Nat2.flatten()
-        nat1 = P @ mean
-        natural = jnp.concatenate((nat1, nat2))
-        return natural
-
-    @classmethod
-    def sample_by_moment(cls, key: PRNGKeyArray, moment: Array, mc_size: int) -> Array:
-        mean, P = cls.moment_to_canon(moment)
-        cov = _inv(P)
-        return jrandom.multivariate_normal(key, mean, cov, shape=(mc_size,))  # It seems JAX does reparameterization trick
-
-    @classmethod
-    def moment_to_canon(cls, moment: Array) -> tuple[Array, Array]:
-        n = jnp.size(moment)
-        m = cls.variable_size(n)
-        mean, p = jnp.split(moment, [m])
-        P = jnp.reshape(p, (m, m))
-        return mean, P
     
     @classmethod
-    def variable_size(cls, param_size: int) -> int:
-        """
-        Get the variable size given a parameter vector size
-        """
-        # n: size of vectorized mean param
-        # m: size of random variable
+    def noise_moment(cls, noise_cov) -> Array:
+        return jnp.diag(1 / noise_cov)
 
-        # n = m + m*m
-        # m = (sqrt(1 + 4n) - 1) / 2. See doc for simpler solution m = floor(sqrt(n)).
-        return int(math.sqrt(param_size))
 
+class LoRaMVN(FullMVN):
     @classmethod
-    def canon_to_moment(cls, mean: Array, P: Array) -> Array:
-        p = P.flatten()
-        moment = jnp.concatenate((mean, p))
-        return moment
-    
-    @classmethod
-    def kl(cls, moment1: Array, moment2: Array) -> Scalar:
-        m1, P1 = cls.moment_to_canon(moment1)
-        m2, P2 = cls.moment_to_canon(moment2)
-        return tfp.kl_divergence(tfp.MultivariateNormalFullCovariance(m1, _inv(P1)), tfp.MultivariateNormalFullCovariance(m2, _inv(P2)))
-
-    @classmethod
-    def param_size(cls, state_dim: int) -> int:
-        return state_dim + state_dim * state_dim
-
-    @classmethod
-    def prior_natural(cls, state_dim) -> Array:
-        moment = cls.canon_to_moment(jnp.zeros(state_dim), jnp.eye(state_dim))
-        return cls.moment_to_natural(moment)
-
-    @classmethod
-    def full_cov(cls, P: Array) -> Array:
-        return _inv(P)
-    
-    @classmethod
-    def constrain_natural(cls, unconstrained: Array) -> Array:
+    def constrain_moment(cls, unconstrained: Array) -> Array:
         n = jnp.size(unconstrained)
         # n = m + 1 + m
         m = (n - 1) // 2
 
-        nat1, diag, nat2 = jnp.split(unconstrained, [m, m + 1])
-        L =  jnp.outer(nat2, nat2)
-        N2 = - (jnn.softplus(diag) * jnp.eye(m) + L)  # ND
-        nat2 = N2.flatten()
-        return jnp.concatenate((nat1, nat2))
+        loc, diag, lora = jnp.split(unconstrained, [m, m + 1])
+        L =  jnp.outer(lora, lora)
+        V = jnn.softplus(diag) * jnp.eye(m) + L
+        v = V.flatten()
+        return jnp.concatenate((loc, v))
 
     @classmethod
     def get_encoders(cls, observation_dim, state_dim, depth, width, key) -> tuple:
@@ -326,10 +260,10 @@ class DiagMVN(MVN):
         return jnp.diag(cov)
 
     @classmethod
-    def constrain_natural(cls, unconstrained) -> Array:
-        nat1, nat2 = jnp.split(unconstrained, 2)
-        nat2 = -jnn.softplus(nat2)
-        return jnp.concatenate((nat1, nat2))
+    def constrain_moment(cls, unconstrained) -> Array:
+        loc, v = jnp.split(unconstrained, 2)
+        v = jnn.softplus(v)
+        return jnp.concatenate((loc, v))
 
     @classmethod
     def get_encoders(cls, observation_dim, state_dim, width, depth, key) -> tuple:
@@ -345,3 +279,7 @@ class DiagMVN(MVN):
             key=back_key,
         )
         return obs_enc, back_enc
+
+    @classmethod
+    def noise_moment(cls, noise_cov) -> Array:
+        return noise_cov

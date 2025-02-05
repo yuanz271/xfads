cfc0cf5f7d4614f31373c41e03387fc03a3db74e
yuanz <yuanz271@gmail.com>
2025-02-05 10:50:08 -0500

    fix reversed dynamics
    Backward starts at predicted z[T+1].

--- src/xfads/core.py
+++ src/xfads/core.py
@@ -19,7 +19,7 @@ def filter(
     u: Array,
     hyperparam: Hyperparam,
 ) -> tuple[Array, Array]:
-    dynamics, statenoise, likelihood, obs_to_update, back_encoder = modules
+    dynamics, likelihood, obs_to_update, back_encoder = modules
 
     approx = hyperparam.approx
     natural_to_moment = jax.vmap(approx.natural_to_moment)
@@ -35,7 +35,7 @@ def filter(
     expected_moment = partial(
         sample_expected_moment,
         forward=dynamics,
-        noise=statenoise,
+        noise=dynamics,
         approx=approx,
         mc_size=hyperparam.mc_size,
     )
@@ -70,7 +70,7 @@ def smooth(
     u: Array,
     hyperparam: Hyperparam,
 ):
-    dynamics, statenoise, likelihood, obs_to_update, back_encoder = modules
+    dynamics, likelihood, obs_to_update, back_encoder = modules
     approx = hyperparam.approx
     natural_to_moment = jax.vmap(approx.natural_to_moment)
     
@@ -82,7 +82,7 @@ def smooth(
     nature_f_1 = nature_prior_1 + update_obs[0]
     moment_f_1 = approx.natural_to_moment(nature_f_1)
 
-    expected_moment = partial(sample_expected_moment, forward=dynamics, noise=statenoise, approx=approx, mc_size=hyperparam.mc_size)
+    expected_moment = partial(sample_expected_moment, forward=dynamics, noise=dynamics, approx=approx, mc_size=hyperparam.mc_size)
 
     def forward(carry, obs):
         key, nature_f_tm1 = carry
@@ -144,57 +144,62 @@ def bismooth(
     Pm = Pfmf + Pbmb
     See Dowling23 Eq.(21-23)
     """
-    forward_dynamics, statenoise, likelihood, obs_to_update, backward_dynamics = modules
+    forward_dynamics, likelihood, obs_to_update, backward_dynamics = modules
     approx = hyperparam.approx
     natural_to_moment = jax.vmap(approx.natural_to_moment)
-    expected_moment_forward = partial(sample_expected_moment, f=forward_dynamics, noise=forward_dynamics, approx=approx, mc_size=hyperparam.mc_size, reverse=False)
-    expected_moment_backward = partial(sample_expected_moment, f=backward_dynamics, noise=backward_dynamics, approx=approx, mc_size=hyperparam.mc_size, reverse=False)    
-    # expected_moment_backward = lambda k, m, u: backward_dynamics(jnp.concatenate((m, u), axis=-1))
-    # expected_moment_backward = partial(sample_expected_moment, f=forward_dynamics, noise=forward_dynamics, approx=approx, mc_size=hyperparam.mc_size, reverse=True)    
+    expected_moment_forward = partial(sample_expected_moment, f=forward_dynamics, noise=forward_dynamics, approx=approx, mc_size=hyperparam.mc_size)
+    expected_moment_backward = partial(sample_expected_moment, f=backward_dynamics, noise=backward_dynamics, approx=approx, mc_size=hyperparam.mc_size)    
     
-    info_obs = jax.vmap(obs_to_update)(y)
-    update_obs = jax.vmap(approx.constrain_natural)(info_obs)
+    update_by_obs = jax.vmap(approx.constrain_natural)(jax.vmap(obs_to_update)(y))
 
     nature_prior = approx.prior_natural(hyperparam.state_dim)
     
-    nature_f_1 = nature_prior + update_obs[0]
-    nature_b_T = nature_prior + update_obs[-1]
+    nature_f_1 = nature_prior + update_by_obs[0]
 
-    def ff(carry, obs):
+    def ff(carry, obs, expected_moment):
         key, nature_f_tm1 = carry
-        subkey, key_t = jrandom.split(key, 2)
+        key_tp1, key_t = jrandom.split(key, 2)
         update_obs_t, u = obs
-        moment_s_tm1 = approx.natural_to_moment(nature_f_tm1)
-        moment_p_t = expected_moment_forward(key_t, moment_s_tm1, u)
+        moment_f_tm1 = approx.natural_to_moment(nature_f_tm1)
+        moment_p_t = expected_moment(key_t, moment_f_tm1, u)
         nature_p_t = approx.moment_to_natural(moment_p_t)
         nature_f_t = nature_p_t + update_obs_t
-        return (subkey, nature_f_t), (moment_p_t, nature_p_t, nature_f_t)
+        return (key_tp1, nature_f_t), (moment_p_t, nature_p_t, nature_f_t)
 
-    def bf(carry, obs):
-        key, nature_f_tm1 = carry
-        subkey, key_t = jrandom.split(key, 2)
-        update_obs_t, u = obs
-        moment_s_tm1 = approx.natural_to_moment(nature_f_tm1)
-        moment_p_t = expected_moment_backward(key_t, moment_s_tm1, u)
-        nature_p_t = approx.moment_to_natural(moment_p_t)
-        nature_f_t = nature_p_t + update_obs_t
-        return (subkey, nature_f_t), (moment_p_t, nature_p_t, nature_f_t)
+    # def bf(carry, obs):
+    #     key, nature_f_tm1 = carry
+    #     subkey, key_t = jrandom.split(key, 2)
+    #     update_obs_t, u = obs
+    #     moment_f_tm1 = approx.natural_to_moment(nature_f_tm1)
+    #     moment_p_t = expected_moment_backward(key_t, moment_f_tm1, u)
+    #     nature_p_t = approx.moment_to_natural(moment_p_t)
+    #     nature_f_t = nature_p_t + update_obs_t
+    #     return (subkey, nature_f_t), (moment_p_t, nature_p_t, nature_f_t)
 
-    # forward
+    # Forward
     key, forward_key = jrandom.split(key, 2)
     _, (_, _, nature_f) = scan(
-        ff, init=(forward_key, nature_f_1), xs=(update_obs[1:], u[:-1])  # t = 2 ...
-    ) 
+        partial(ff, expected_moment=expected_moment_forward), init=(forward_key, nature_f_1), xs=(update_by_obs[1:], u[:-1])  # t = 2 ... T+1
+    )
     nature_f = jnp.vstack((nature_f_1, nature_f))  # 1...T
 
     ## Backward
+    key, subkey = jrandom.split(key, 2)
+    (_, nature_b_Tp1), _ = ff((subkey, nature_f[-1]), (jnp.zeros_like(nature_prior), u[-1]), expected_moment_forward)
+    # nature_f_T = nature_f[-1]
+    # moment_f_T = approx.natural_to_moment(nature_f_T)
+    # moment_p_Tp1 = expected_moment_forward(subkey, moment_f_T, u[-1])
+    # nature_p_Tp1 = approx.moment_to_natural(moment_p_Tp1)
+
+    # nature_b_Tp1 = nature_f[-1]
+
     key, backward_key = jrandom.split(key, 2)
     _, (_, nature_p_b, _) = scan(
-        bf, init=(backward_key, nature_b_T), xs=(update_obs[:-1], u[:-1]), reverse=True
+        partial(ff, expected_moment=expected_moment_backward), init=(backward_key, nature_b_Tp1), xs=(update_by_obs, u), reverse=True
     )
     # nature_p_b[1: T-1]
-    nature_p_b = jnp.vstack((nature_p_b, nature_prior))  # 1...T
-    nature_s = nature_f + nature_p_b - nature_prior[None, ...]
+    # nature_p_b = jnp.vstack((nature_p_b, nature_prior))  # 1...T, smoothing distribution of z[T] equals to its filtering distribution
+    nature_s = nature_f + nature_p_b - jnp.expand_dims(nature_prior, axis=0)
     moment_s = natural_to_moment(nature_s)
     
     # expectation should be under smoothing distribution
@@ -205,59 +210,6 @@ def bismooth(
     return nature_s, moment_s, moment_p
 
 
-
-# def smooth(
-#     modules: PyTree,
-#     key: PRNGKeyArray,
-#     t: Array,
-#     y: Array,
-#     u: Array,
-#     nature_f: Array,
-#     hyperparam: Hyperparam,
-# ):
-#     dynamics, statenoise, likelihood, obs_to_update, back_encoder = modules
-#     approx = hyperparam.approx
-#     natural_to_moment = jax.vmap(approx.natural_to_moment)
-
-#     expected_moment = partial(
-#         sample_expected_moment,
-#         forward=dynamics,
-#         noise=statenoise,
-#         approx=approx,
-#         mc_size=hyperparam.mc_size,
-#     )
-
-#     def backward(carry, z):
-#         key, nature_s_tp1 = carry
-#         subkey, key_t = jrandom.split(key, 2)
-#         info_y_t, nature_f_t = z
-#         update = approx.moment_to_natural(
-#             approx.constrain_moment(
-#                 back_encoder(jnp.concatenate((info_y_t, nature_s_tp1)))
-#             )
-#         )
-#         nature_s_t = nature_f_t + update
-#         return (subkey, update), nature_s_t
-
-#     ## Backward
-#     nature_s_T = nature_f[-1]
-#     key, backward_key = jrandom.split(key, 2)
-#     _, nature_s = scan(
-#         backward,
-#         init=(backward_key, nature_s_T),
-#         xs=(y[:-1], nature_f[:-1]),
-#         reverse=True,
-#     )  # reverse both xs and the output
-#     nature_s = jnp.vstack((nature_s, nature_s_T))
-#     moment_s = natural_to_moment(nature_s)
-
-#     keys = jrandom.split(key, jnp.size(moment_s, 0))
-#     moment_p = jax.vmap(expected_moment)(keys, moment_s, u)
-#     moment_p = jnp.vstack((moment_s[0], moment_p[:-1]))
-
-#     return nature_s, moment_s, moment_p
-
-
 def smooth_pseudo(
     modules: PyTree,
     key: PRNGKeyArray,
@@ -266,7 +218,7 @@ def smooth_pseudo(
     u: Array,
     hyperparam: Hyperparam,
 ):
-    dynamics, statenoise, likelihood, obs_to_update, pseudo_encoder = modules
+    dynamics, likelihood, obs_to_update, pseudo_encoder = modules
     approx = hyperparam.approx
     natural_to_moment = jax.vmap(approx.natural_to_moment)
 
@@ -286,7 +238,7 @@ def smooth_pseudo(
     expected_moment = partial(
         sample_expected_moment,
         forward=dynamics,
-        noise=statenoise,
+        noise=dynamics,
         approx=approx,
         mc_size=hyperparam.mc_size,
     )

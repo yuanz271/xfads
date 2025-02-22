2267d09b0d05b00c41e9027befbfb142e2fd5f4b
yz <yuanz271@gmail.com>
2025-02-22 13:19:33 -0500

    change name accordingly

--- src/xfads/vi.py
+++ src/xfads/vi.py
@@ -2,10 +2,10 @@ from typing import Callable, Type
 
 from jaxtyping import Array, PRNGKeyArray, Scalar
 
-from .distributions import MVN
+from .distributions import Approx
 
 
-def elbo(key: PRNGKeyArray, t: Array, moment: Array, moment_p: Array, y: Array, eloglik: Callable[..., Scalar], approx: Type[MVN], *, mc_size: int) -> Scalar:
+def elbo(key: PRNGKeyArray, t: Array, moment: Array, moment_p: Array, y: Array, eloglik: Callable[..., Scalar], approx: Type[Approx], *, mc_size: int) -> Scalar:
     """Single time point"""
     ell: Scalar = eloglik(key, t, moment, y, approx, mc_size)
     kl: Scalar = approx.kl(moment, moment_p)
